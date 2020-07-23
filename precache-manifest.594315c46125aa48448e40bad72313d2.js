c32ce284da1a20e91e94c5310f24e39350d9a1d4
pranoyk <pranoy1998k@gmail.com>
2020-07-23 18:54:24 +0530

    Updates

---
+++ precache-manifest.594315c46125aa48448e40bad72313d2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ea11c1646c7e19c165a0382793d2818",
+    "url": "/index.html"
+  },
+  {
+    "revision": "561af42890853fbf13b8",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "79e86f1bd1bd6888936f",
+    "url": "/static/js/2.293cf025.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.293cf025.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "561af42890853fbf13b8",
+    "url": "/static/js/main.1d458ab8.chunk.js"
+  },
+  {
+    "revision": "b139d789d8ac7a484f79",
+    "url": "/static/js/runtime-main.2212405e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
