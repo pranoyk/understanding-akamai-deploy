a8c55890e39d6a01847e7a96383bbf3203830483
pranoyk <pranoy1998k@gmail.com>
2020-07-23 19:44:16 +0530

    remove png and svg files

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src="https://res.cloudinary.com/dm6jlqm9r/image/upload/v1595512760/logo.5d5d9eef_glz7fd.svg" className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
